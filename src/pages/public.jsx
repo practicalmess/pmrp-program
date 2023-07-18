8357ad8c2f4c90029a2ac4152dbd1e31c3b6fed1
Sidney Hackney <snackney@gmail.com>
2023-07-18 18:02:10 -0400

    Remove build files and redirect from public path

---
+++ src/pages/public.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Redirect = () => {
+  window.location.replace('https://practicalmess.github.io/pmrp-program/');
+  return (
+    <div></div>
+  );
+};
+
+export default Redirect;
