2030a9789c180d2256073bd729d90ee6bc2bd8dd
Sidney Hackney <snackney@gmail.com>
2023-07-20 20:13:50 -0400

    Fix conflicts hopefully

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,7 @@ import React, { useRef, useEffect } from "react";
 import CreditsBlock from "../components/CreditsBlock.js";
 import Bios from "../components/Bios.js";
 import ResponsiveHeroImage from "../components/ResponsiveHeroImage.js";
-import useWindowDimensions from "../utilities/useWindowDimensions.js";
-import * as Scroll from "react-scroll";
 import {
-  Link,
-  Button,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
   scroller,
 } from "react-scroll";
 import { data } from "../data.js";
